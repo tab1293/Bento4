3513fbde27850dc856bddf332ca211eb8c01613f
Tom Berger <tab1293@gmail.com>
2020-04-27 17:04:18 -0400

    Update sdkpackager

--- Scripts/SdkPackager.py
+++ Scripts/SdkPackager.py
@@ -35,27 +35,14 @@ def GetVersion():
 # GetSdkRevision
 #############################################################
 def GetSdkRevision():
-    cmd = 'git status --porcelain -b'
+    cmd = "git rev-parse HEAD"
     lines = os.popen(cmd).readlines()
-    branch = ''
-    if not lines[0].startswith('## master'):
-        print('WARNING: not on master branch')
-        branch = '+' + lines[0][3:].strip()
-    if len(lines) > 1:
+    print(lines)
+    if len(lines) != 1:
         print('ERROR: git status not empty')
-        print(''.join(lines))
         return None
 
-    cmd = 'git tag --contains HEAD'
-    tags = os.popen(cmd).readlines()
-    if len(tags) != 1:
-        print('ERROR: expected exactly one tag for HEAD, found', len(tags), ':', tags)
-        return None
-    version = tags[0].strip()
-    sep = version.find('-')
-    if sep < 0:
-        print('ERROR: unrecognized version string format:', version)
-    return version[sep+1:] + branch
+    return lines[0].strip()
 
 #############################################################
 # File Copy
